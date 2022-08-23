b095f4ebf4ac5c0d1868f36ee374a6cc59f32861
Borysov Kostyantyn <koss199710@gmail.com>
2022-08-23 16:04:40 +0300

    Визначення об'ему та сумми всіх довжин(ребер) паралелепіеда

--- src/main/java/com/hillel/borysov/lessons/lesson3/Main.java
+++ src/main/java/com/hillel/borysov/lessons/lesson3/Main.java
@@ -2,12 +2,20 @@ package com.hillel.borysov.lessons.lesson3;
 
 public class Main {
     public static void main(String[] args) {
-    areaOfParallelepiped(10,9, 5);
+        areaOfParallelepiped(10, 9, 5);
+        lengthAllSidesOfParallelepiped(10,9, 5);
     }
 
     public static void areaOfParallelepiped(int lengthSide_A, int widthSide_B, int heightSide_C) {
-    int volume = lengthSide_A * widthSide_B * heightSide_C;
-        System.out.println("\"Об'єм паралелепіпеда = " + volume + " cm" + '\u00B3' + "\"");
+        int volume = lengthSide_A * widthSide_B * heightSide_C;
+        System.out.println("\"Об'єм паралелепіпеда = " + volume + " см" + '\u00B3' + "\"");
+
+    }
+
+    public static void  lengthAllSidesOfParallelepiped(int lengthSide_A, int widthSide_B, int heightSide_C) {
+        int lengthAllSides = 4 * (lengthSide_A + widthSide_B + heightSide_C);
+
+        System.out.println("\"Сумарна довжина всіх сторін(ребер) паралелепіпеда: " + lengthAllSides + " см\"");
 
     }
 }
