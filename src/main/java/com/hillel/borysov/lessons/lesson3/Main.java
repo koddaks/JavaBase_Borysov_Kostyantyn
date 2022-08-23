8c64639a38e01fef3189888287e48a6f3dc618cb
Borysov Kostyantyn <koss199710@gmail.com>
2022-08-23 15:49:58 +0300

    Визначння об'ему паралелепіеда

--- src/main/java/com/hillel/borysov/lessons/lesson3/Main.java
+++ src/main/java/com/hillel/borysov/lessons/lesson3/Main.java
@@ -2,8 +2,12 @@ package com.hillel.borysov.lessons.lesson3;
 
 public class Main {
     public static void main(String[] args) {
-        String northLatitude = "48" + '\u00B0' + "51`29\" north latitude";
-        String eastLongitude = " 2" + '\u00B0' + "17`40\" east longitude.";
-        System.out.println("Eiffel Tower coordinates is: " + northLatitude + eastLongitude);
+    areaOfParallelepiped(10,9, 5);
+    }
+
+    public static void areaOfParallelepiped(int lengthSide_A, int widthSide_B, int heightSide_C) {
+    int volume = lengthSide_A * widthSide_B * heightSide_C;
+        System.out.println("\"Об'єм паралелепіпеда = " + volume + " cm" + '\u00B3' + "\"");
+
     }
 }
