73dd3de2267a89b5762f9213cdbf745baff9bcde
Borysov Kostyantyn <koss199710@gmail.com>
2022-08-25 16:06:53 +0300

    Метод String.format() и класс StringFormatter

--- src/main/java/com/hillel/borysov/lessons/examples/StringFormat.java
+++ src/main/java/com/hillel/borysov/lessons/examples/StringFormat.java
@@ -2,11 +2,13 @@ package com.hillel.borysov.lessons.examples;
 
 public class StringFormat {
     public static void main(String[] args) {
-        System.out.println(format("Амиго", 5000));
+        System.out.println(format("Maks", 800));
     }
 
     public static String format(String name, int salary) {
         String phrase = "Меня зовут %s. Я буду зарабатывать %d$ в месяц.";
+        System.out.println(phrase);
+
 
         return String.format(phrase, name, salary);
     }
