b91faa9431e563820dd4d76d988af13717aa97fb
Borysov Kostyantyn <koss199710@gmail.com>
2022-08-29 21:11:23 +0300

    Lesson 4.Dynasty

---
+++ src/main/java/com/hillel/borysov/lessons/lesson4/Main.java
@@ -0,0 +1,30 @@
+package com.hillel.borysov.lessons.lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+        String li = "Li";
+        final double totalNumberOfWarriorsLi = 860;
+
+        double warriorLiAttack = 13;
+        double archerLiAttack = 24;
+        double riderLiAttack = 46;
+
+        final String min = "Min";
+        final double coefficient = 1.5;
+        final double totalNumberOfWarriorsMin = 860 * coefficient;
+
+        double warriorMinAttack = 13;
+        double archerMinAttack = 24;
+        double riderMinAttack = 46;
+
+        totalAttack(li,warriorLiAttack,archerLiAttack,riderLiAttack,totalNumberOfWarriorsLi);
+        totalAttack(min,warriorMinAttack,archerMinAttack,riderMinAttack,totalNumberOfWarriorsMin);
+
+    }
+
+    public static void totalAttack(String army,double warriorAttack,double archerAttack,double riderAttack ,double totalNumberOfWarriors) {
+        double totalAttackValue = (warriorAttack + archerAttack + riderAttack) * totalNumberOfWarriors;
+        System.out.println("Загальна атка війська " + army + ":" + totalAttackValue);
+
+    }
+}
